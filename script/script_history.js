0a8a8adf9f159f05b99ab7fa88e522f4bcc32cf7
siromomo <365137154@qq.com>
2019-03-05 20:10:05 +0800

    changed the path of some sources

--- script/script_history.js
+++ script/script_history.js
@@ -1,23 +1,23 @@
 var footprintString = getCookie("footprint");
 if(footprintString === null || footprintString === undefined || footprintString.length === 0)
-    footprintString = "<a href='index.html' style='color: #ff5555'>首页</a>";
+    footprintString = "<a href='../index.html' style='color: #ff5555'>首页</a>";
 var pageTitle = document.title;
 var stringToAdd = "";
 switch (pageTitle) {
     case "Art Shop - Painting Item":
-        stringToAdd = " - <a href='item.html' style='color: #ff5555'>商品详情</a>";
+        stringToAdd = " - <a href='../item.html' style='color: #ff5555'>商品详情</a>";
         break;
     case "Art Shop - Personal Info":
-        stringToAdd = " - <a href='info.html' style='color: #ff5555'>个人信息</a>";
+        stringToAdd = " - <a href='../info.html' style='color: #ff5555'>个人信息</a>";
         break;
     case "Art Shop - Search Page":
-        stringToAdd = " - <a href='search.html' style='color: #ff5555'>搜索</a>";
+        stringToAdd = " - <a href='../search.html' style='color: #ff5555'>搜索</a>";
         break;
     case "Art Shop - Shopping Cart":
-        stringToAdd = " - <a href='shoppingCart.html' style='color: #ff5555'>购物车</a>";
+        stringToAdd = " - <a href='../shoppingCart.html' style='color: #ff5555'>购物车</a>";
         break;
     default:
-        footprintString = "<a href='index.html' style='color: #ff5555'>首页</a>"
+        footprintString = "<a href='../index.html' style='color: #ff5555'>首页</a>"
 }
 if(stringToAdd !== "") {
     let lastIndex = footprintString.indexOf(stringToAdd);
